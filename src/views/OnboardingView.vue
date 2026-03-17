b56b61633236ef564b03e441520356204c1eb2f9
rachit74 <rachithooda09@gmail.com>
2026-03-17 14:43:37 +0530

    axios import fix

--- src/views/OnboardingView.vue
+++ src/views/OnboardingView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { reactive, ref, watch } from 'vue'
 import { api } from '@/config'
+import axios from 'axios'
 
 const CLOUDINARY_CLOUD_NAME = import.meta.env.VITE_CLOUDINARY_CLOUD_NAME
 const CLOUDINARY_UPLOAD_PRESET = import.meta.env.VITE_CLOUDINARY_UPLOAD_PRESET
