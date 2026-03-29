10ca2f54f04b126aee4bf72a4d7d5e610cb47588
rachit74 <rachithooda09@gmail.com>
2026-03-29 09:35:06 +0530

    fix base url

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const BASE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://102d-2401-4900-1c30-1cf2-493b-a63b-cbd4-32f9.ngrok-free.app'
 
 // Axios instance with ngrok header to skip browser warning
 export const api = axios.create({
