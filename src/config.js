e4c9f7496bb216eccebf7439b004df2b4aa8f371
rachit74 <rachithooda09@gmail.com>
2026-04-03 22:51:53 +0530

    maintenance page

--- src/config.js
+++ src/config.js
@@ -2,6 +2,13 @@ import axios from 'axios'
 
 export const BASE_URL = 'https://102d-2401-4900-1c30-1cf2-493b-a63b-cbd4-32f9.ngrok-free.app'
 
+// Maintenance mode configuration
+// Set to true to show maintenance page to all users
+export const MAINTENANCE_MODE = false
+
+// Routes that are accessible during maintenance mode
+export const MAINTENANCE_EXEMPT_ROUTES = ['/maintenance']
+
 // Axios instance with ngrok header to skip browser warning
 export const api = axios.create({
   baseURL: BASE_URL,
