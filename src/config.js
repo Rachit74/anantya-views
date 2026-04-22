dbed9ab149e03c63a1e4abe6194fda2f86794881
rachit74 <rachithooda09@gmail.com>
2026-04-22 22:21:47 +0530

    update maintain status to false

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ export const BASE_URL = 'https://dc1a-2401-4900-1c30-1cf2-ed76-bf96-b6b2-7269.ng
 
 // Maintenance mode configuration
 // Set to true to show maintenance page to all users
-export const MAINTENANCE_MODE = true
+export const MAINTENANCE_MODE = false
 
 // Routes that are accessible during maintenance mode
 export const MAINTENANCE_EXEMPT_ROUTES = ['/maintenance']
